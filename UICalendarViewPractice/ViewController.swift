480c5bba525fc8c7d516a25b3425a61b8d717930
EhsanTgv <EhsanTgv@gmail.com>
2023-01-29 23:45:22 +0300

    Implement calendar

--- UICalendarViewPractice/ViewController.swift
+++ UICalendarViewPractice/ViewController.swift
@@ -11,9 +11,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        createCalendar()
     }
 
-
+    func createCalendar() {
+        view.backgroundColor = .systemPink
+        let calendarView = UICalendarView()
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+        
+        calendarView.calendar = .current
+        calendarView.locale = .current
+        calendarView.fontDesign = .rounded
+        calendarView.layer.cornerRadius = 12
+        calendarView.backgroundColor = .systemBackground
+        calendarView.delegate = self
+        
+        view.addSubview(calendarView)
+        
+        NSLayoutConstraint.activate([
+            calendarView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            calendarView.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -10),
+            calendarView.heightAnchor.constraint(equalToConstant: 300),
+            calendarView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+        ])
+    }
 }
 
+extension ViewController: UICalendarViewDelegate {
+    func calendarView(_ calendarView: UICalendarView, decorationFor dataComponents: DateComponents) -> UICalendarView.Decoration? {
+        return nil
+    }
+}
